3c255eb6339d125b0efaff8e6011e2b4b23f4f54
tushant-akar <tushantakar10@gmail.com>
2024-08-08 02:57:06 +0530

    Profile Fragment Done

--- app/src/main/java/com/example/yardsync/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/yardsync/ui/fragments/ProfileFragment.kt
@@ -1,18 +1,47 @@
 package com.example.yardsync.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.yardsync.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.example.yardsync.databinding.FragmentProfileBinding
+import com.example.yardsync.viewModel.AuthViewModel
+import kotlinx.coroutines.launch
 
 class ProfileFragment : Fragment() {
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var viewModel: AuthViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        viewModel = AuthViewModel(requireContext())
+        retrieveEmployee()
+
+        binding.logoutBtn.setOnClickListener {
+            viewModel.logout()
+        }
+        return binding.root
+    }
+
+    private fun retrieveEmployee() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            val emp = viewModel.getEmployee(requireContext())
+            binding.edtName.setText(emp?.name)
+            binding.edtEmail.setText(emp?.email)
+            binding.edtWarehouse.setText(emp?.warehouse)
+            binding.edtID.setText(emp?.empId)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
